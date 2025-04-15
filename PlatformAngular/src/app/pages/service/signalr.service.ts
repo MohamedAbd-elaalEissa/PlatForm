1b110b1a4ce9a961f5bbabafd2264a74bbd9d015
MohamedAbd-elaalEissa <166083907+MohamedAbd-elaalEissa@users.noreply.github.com>
2025-04-15 22:35:40 +0200

    Change To IIS

--- PlatformAngular/src/app/pages/service/signalr.service.ts
+++ PlatformAngular/src/app/pages/service/signalr.service.ts
@@ -17,7 +17,7 @@ export class SignalrService {
     debugger
     this.hubConnection = new signalR.HubConnectionBuilder()
 
-      .withUrl(`https://localhost:7056/notificationHub?email=${email}`, {
+      .withUrl(`https://localhost:44305/notificationHub?email=${email}`, {
         withCredentials: true // this matches the CORS settings on the server
       })
       .build();
