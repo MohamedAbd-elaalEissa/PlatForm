8cb8d002b58c4e889aba2c9bd7844409c4046079
MohamedAbd-elaalEissa <166083907+MohamedAbd-elaalEissa@users.noreply.github.com>
2025-04-28 01:26:08 +0300

    Blur

--- PlatformAngular/src/app.component.ts
+++ PlatformAngular/src/app.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SignalrService } from './app/pages/service/signalr.service';
+import { WatermarkComponent } from './app/pages/watermark/watermark.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule],
-    template: `<router-outlet></router-outlet>`
+    imports: [RouterModule, WatermarkComponent],
+    template: ` 
+    <app-watermark></app-watermark>
+    <router-outlet></router-outlet>`
 })
 export class AppComponent {
-   
+
     constructor(private signalRService: SignalrService) {
-        
+
     }
     ngOnDestroy(): void {
         // Stop SignalR connection when component is destroyed
         this.signalRService.stopConnection();
-      }
+    }
 }
